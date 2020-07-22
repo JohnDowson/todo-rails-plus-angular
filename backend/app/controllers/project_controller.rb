0c7e22916da6ac72fe3f666c9535d3632d629b0d
Ivan Chinenov <ichinenov@gmail.com>
2020-07-22 12:07:58 +0300

    Add endpoint for project creation

--- backend/app/controllers/project_controller.rb
+++ backend/app/controllers/project_controller.rb
@@ -2,4 +2,8 @@ class ProjectController < ApplicationController
   def index
     render json: Project.all.map { |proj| proj.json }
   end
+  def create
+    project = params.require(:project).permit(:text)
+    render json: Project.create(project).json
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/projects', to: 'project#index'
+  post 'projects', to: 'project#create'
   post '/todos', to: 'todo#create'
   patch '/todos/:id', to: 'todo#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
